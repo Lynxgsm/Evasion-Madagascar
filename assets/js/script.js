f39fa0a124d55905b01e77a8cc9a75c12da44d5c
lynxinternet <aili.fida.ac@gmail.com>
2017-05-23 12:50:58 +0200

    intend

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,12 @@
     app.config(function($routeProvider) {
         $routeProvider
             .when("/", { templateUrl: "assets/pages/home.html" })
-            .when("/destination", { templateUrl: "assets/pages/destination.html" })
-            .when("/ville", { templateUrl: "assets/pages/ville.html" })
-            .when("/prestataire", { templateUrl: "assets/pages/prestataire.html" })
+            .when("/destination", { templateUrl: "assets/pages/destination.html", controller: "destinationController" })
+            .when("/ville", { templateUrl: "assets/pages/ville.html", controller: "villeController" })
+            .when("/prestataire", { templateUrl: "assets/pages/prestataire.html", controller: "prestataireController" })
+            .when("/voyage", { templateUrl: "assets/pages/voyage.html", controller: "voyageController" })
+            .when("/inscription", { templateUrl: "assets/pages/inscription.html", controller: "inscriptionController" })
+            .when("/user", { templateUrl: "assets/pages/user.html", controller: "userController" })
             .otherwise({ redirectTo: "/" })
     });
 
@@ -22,7 +25,23 @@
     });
 
     app.controller('villeController', function($rootScope) {
+        $rootScope.titre = "III- Sélection ville";
+    });
 
+    app.controller('prestataireController', function($rootScope) {
+        $rootScope.titre = "IV- Préstataires";
+    });
+
+    app.controller('voyageController', function($rootScope) {
+        $rootScope.titre = "V- Voyages";
+    });
+
+    app.controller('inscriptionController', function($rootScope) {
+        $rootScope.titre = "VI- Inscription";
+    });
+
+    app.controller('userController', function($rootScope) {
+        $rootScope.titre = "VII- Page Utilisateur";
     });
 
 })();
